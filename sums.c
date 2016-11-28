ee4ddd454b4c75a921ae8ee05806ff5acb50f7d6
jedimaster7 <horizonmediaceo@gmail.com>
2016-11-27 16:21:56 -0800

    updated sums.c
    TODO: clean up code; add user input

--- sums.c
+++ sums.c
@@ -1,34 +1,53 @@
 // simple program to find Riemann sums
+// bad coding style
+// need to clean up
+// Copyright Andrew Tolan 2016
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
 int main (void)
 {
+  // main variables
   float yValue;
-  float rectValue = 0;
+  float rectValue1 = 0; //for left hand limit
+  float rectValue2 = 0; //for right hand limit
   float i;
-
-  // for upper limit
-  float upperLimit()
-  {
-    for (i = 1; i < 2; i += 0.2)
-    {
-      yValue = 7 / i;
   
-      rectValue += yValue * 0.2;
+  // changeable variables
+  float start = 1; //change to current
+  float end = 2; //change to current
+  float increment = 0.2; //change to current
+
+  // left hand limit
+  float leftLimit()
+  {
+    for (i = start; i < end; i += increment) 
+    {
+      yValue = 7 / i; // change to current equation
+  
+      rectValue1 += yValue * increment;
     }
   
-    printf("%f", rectValue);
+    printf("Left hand limit: %f\n", rectValue1);
   }
   
-  // for lower limit
-  float lowerLimit()
+  // right hand limit
+  float rightLimit()
   {
-    for (i = 2; i >= 1; i -= 0.2)
+    for (i = end; i >= start; i -= increment) // must be opposite of left hand limit
+    {
+      yValue = 7 / i; //change to current equation
+      
+      rectValue2 += yValue * increment;
+    }
+    
+    printf("Right hand limit: %f\n", rectValue2);
   }
   
-  upperLimit();
+  leftLimit();
+  rightLimit();
 
   return 0;
 }
